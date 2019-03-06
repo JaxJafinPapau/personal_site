b9f8b1f4b1ca65bf3adec84fd6435c2d53d8bfdc
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-03-06 16:52:07 -0700

    points personal site call method to index

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,6 +2,6 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'test/html'}, ['Welcome!']]
+    ['200', {'Content-Type' => 'test/html'}, [File.read('./app/views/index.html')]]
   end
 end
